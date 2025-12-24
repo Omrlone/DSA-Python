aeefe679dd0f421d145374895104efb977ecd92d
Omar Lone <137639742+Omrlone@users.noreply.github.com>
2025-12-25 00:13:30 +0530

    Add files via upload

---
+++ Two Sum problem.py
@@ -0,0 +1,12 @@
+def two_sum(num, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+
+nums = [3, 2, 4]
+target = 6
+
+result = two_sum(nums, target)
+print("indices:", result)
